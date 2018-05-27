544b5b764561ce8eb6455e85575a7670ac4cfed3
YaoLaiGang <2046682604@qq.com>
2018-05-27 18:19:40 +0800

    补充的一些常用的数学函数

---
+++ len_mathfunction.py
@@ -0,0 +1,48 @@
+from math import modf
+'''
+本篇笔记主要讲解Python中常用的数学函数
+2018年5月24日11:26:15
+Peter
+'''
+#第一组 数学函数
+'''
+函数	返回值 ( 描述 )
+abs(x)	返回数字的绝对值，如abs(-10) 返回 10
+ceil(x)	返回数字的上入整数，如math.ceil(4.1) 返回 5
+cmp(x, y)
+
+如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1。 Python 3 已废弃 。使用 使用 (x>y)-(x<y) 替换。
+exp(x)	返回e的x次幂(ex),如math.exp(1) 返回2.718281828459045
+fabs(x)	返回数字的绝对值，如math.fabs(-10) 返回10.0
+floor(x)	返回数字的下舍整数，如math.floor(4.9)返回 4
+log(x)	如math.log(math.e)返回1.0,math.log(100,10)返回2.0
+log10(x)	返回以10为基数的x的对数，如math.log10(100)返回 2.0
+max(x1, x2,...)	返回给定参数的最大值，参数可以为序列。
+min(x1, x2,...)	返回给定参数的最小值，参数可以为序列。
+modf(x)	返回x的整数部分与小数部分，两部分的数值符号与x相同，整数部分以浮点型表示。
+pow(x, y)	x**y 运算后的值。
+round(x [,n])	返回浮点数x的四舍五入值，如给出n值，则代表舍入到小数点后的位数。
+sqrt(x)	返回数字x的平方根。
+'''
+x = modf(100.2) #这里是以元组的方式返回的数据
+
+print(x[1],x[0],round(100.55,1)) #应该是五舍六入
+
+'''
+最好不要使用round函数，如果需要不如使用ceil floor或者  / //
+round函数比较迷，不同的Python版本结果不尽相同
+'''
+
+#随机数函数
+
+'''
+也算是比较常用的函数类
+choice(seq)	从序列的元素中随机挑选一个元素，比如random.choice(range(10))，从0到9中随机挑选一个整数。
+randrange ([start,] stop [,step])	从指定范围内，按指定基数递增的集合中获取一个随机数，基数缺省值为1
+random()	随机生成下一个实数，它在[0,1)范围内。
+seed([x])	改变随机数生成器的种子seed。如果你不了解其原理，你不必特别去设定seed，Python会帮你选择seed。
+shuffle(lst)	将序列的所有元素随机排序
+uniform(x, y)	随机生成下一个实数，它在[x,y]范围内。
+'''
+from math import pi,e
+print(pi,e)
