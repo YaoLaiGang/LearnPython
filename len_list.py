9d15e7b05291f2e6ac9bd77d9efebbb40735dd63
YaoLaiGang <2046682604@qq.com>
2018-05-28 17:24:31 +0800

    list的增删改查内置函数和克隆

--- len_list.py
+++ len_list.py
@@ -33,4 +33,41 @@ print(a)
 3.list使用+拼接
 4.list元素可变
 '''
- 
+ #增加列表元素
+list2.append('add')
+print(list2)
+
+#删除列表元素
+
+del list2[2]    #直接使用del删除
+print(list2)
+
+list2.remove(124)   #使用内置的remove函数删除
+print(list2)
+
+#获得长度
+print(len(list2))
+
+#判断存在
+if 124 in list2:
+    pass
+else:
+    print('list中不存在值124')
+
+#列表的copy
+'''
+列表的浅copy直接赋值即可，缺点是触一发而动全身
+深copy有三种方式
+'''
+list3 = list2[:]
+list3[0] = 'Yao'
+print(list3,list2)
+
+import copy
+list3 = copy.copy(list2)
+
+list3 = list2.copy()
+
+'''
+列表还有一批内置的实用函数，只需查找官方文档即可
+'''
