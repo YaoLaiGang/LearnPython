c5b8853a10f07f96949ab5b81ce33f94c6d3be05
YaoLaiGang <2046682604@qq.com>
2018-09-11 21:27:33 +0800

    运算符重载的实现

--- len_oo.py
+++ len_oo.py
@@ -25,6 +25,7 @@ class Dog:
         self.__sleep()  #内部访问需要self（自指针）指定
         print('汪汪汪')
 
+
     #构造方法
     def __init__(self,weight = 20 , name = '旺财' , gender = 'man'):#默认参数
         self.weight = weight
@@ -61,6 +62,7 @@ class ColorDog(Dog):
     #覆盖父类方法
     def bark(self):
         print('汪汪汪 from black dog')
+ 
     
     def __init__(self , weight = 20 , name = '旺财' , gender = 'man',color = 'black'):
         Dog.__init__(self,weight,name,gender) #调用父类方法,别忘了self
@@ -68,5 +70,22 @@ class ColorDog(Dog):
 
 dogBlack = ColorDog(weight=50,color='blue')
 
-print('我是',dogBlack.name,'我的重量是',dogBlack.weight,'我的性别是',dogBlack.getGender())
+print('我是',dogBlack.name,'我的重量是',dogBlack.weight,'我的性别是',dogBlack.getGender(),'我的颜色是',dogBlack.color)
 dogBlack.bark()
+super(ColorDog,dogBlack).bark()
+
+
+#运算符重载，具有和C++类似的功能
+class Integer:
+    num = 0
+    def __init__(self,num):
+        self.num = num
+    
+    def __add__(self,other):
+        return Integer(self.num+other.num)
+
+    def __str__(self):
+        return str(self.num)
+
+a , b = Integer(1) , Integer(1)
+print(a+b)
